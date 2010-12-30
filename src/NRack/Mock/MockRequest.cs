db85ea79e17fa6a4b7e19f6fe525277c99b92d09
Kevin Swiber <kswiber@gmail.com>
2010-12-30 12:41:04 -0500

    Extracting MockResponse into its own class.

--- src/NRack/Mock/MockRequest.cs
+++ src/NRack/Mock/MockRequest.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
 using System.Text;
-using NRack.Adapters;
 
 namespace NRack.Mock
 {
@@ -227,55 +225,5 @@ namespace NRack.Mock
 
             return env;
         }
-    }
-
-    public class MockResponse
-    {
-        public MockResponse(dynamic[] responseArray)
-        {
-            Status = Convert.ToInt32(responseArray[0]);
-            OriginalHeaders = responseArray[1];
-            Headers = (Hash) responseArray[1];
-
-            var bodyList = new List<string>();
-            var body = responseArray[2] as IResponseBody;
-
-            if (body == null)
-            {
-                body = new EnumerableBodyAdapter(responseArray[2]);
-            }
-
-
-            body.Each(part => bodyList.Add(part.ToString()));
-
-            Body = new EnumerableBodyAdapter(bodyList);
-
-            var errors = responseArray.Length > 3 ? responseArray[3] : new MemoryStream();
-            if (errors is MemoryStream)
-            {
-                var errorStream = (MemoryStream) errors;
-
-                var pos = errorStream.Position;
-                errorStream.Position = 0;
-
-                var reader = new StreamReader(errorStream);
-                var errorString = reader.ReadToEnd();
-
-                errorStream.Position = pos;
-
-                Errors = errorString;
-            }
-        }
-
-        public string this[string name]
-        {
-            get { return Headers[name]; } 
-        }
-
-        public int Status { get; private set; }
-        public Hash OriginalHeaders { get; private set; }
-        public Hash Headers { get; private set; }
-        public IResponseBody Body { get; private set; }
-        public string Errors { get; set; }
     }
 }
