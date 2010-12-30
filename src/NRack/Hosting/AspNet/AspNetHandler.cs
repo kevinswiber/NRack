186604a4132747514f4e6ba17c9969a6465d9010
Kevin Swiber <kswiber@gmail.com>
2010-12-30 15:45:07 -0500

    Simplifying startup config.

--- src/NRack/Hosting/AspNet/AspNetHandler.cs
+++ src/NRack/Hosting/AspNet/AspNetHandler.cs
@@ -1,19 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Reflection;
 using System.Web;
+using System.Web.Compilation;
+using NRack.Configuration;
 
 namespace NRack.Hosting.AspNet
 {
     public class AspNetHandler : IHttpHandler
     {
         public static Func<Builder> GetBuilderInContext;
+        public static RackConfigBase RackConfig;
 
         #region Implementation of IHttpHandler
 
         public void ProcessRequest(HttpContext context)
         {
+            if (RackConfig == null)
+            {
+                RackConfig = GetRackConfigInstance();
+                GetBuilderInContext = () => new Builder(RackConfig.ExecuteRackUp);
+            }
+
             var rawEnvironment = context.Request.Params;
             Dictionary<string, dynamic> environment =
                 rawEnvironment.AllKeys.ToDictionary(key => key, key => (object)rawEnvironment[key]);
@@ -87,5 +97,52 @@ namespace NRack.Hosting.AspNet
         }
 
         #endregion
+
+        private RackConfigBase GetRackConfigInstance()
+        {
+            var rackConfigSection = (RackConfigurationSection)ConfigurationManager.GetSection("rack");
+
+            var rackConfigType = (rackConfigSection != null && !string.IsNullOrEmpty(rackConfigSection.Type))
+                                     ? Type.GetType(rackConfigSection.Type)
+                                     : GetRackConfigTypeFromReferencedAssemblies();
+
+            return ((RackConfigBase)Activator.CreateInstance(rackConfigType));
+        }
+
+        private static Type GetRackConfigTypeFromReferencedAssemblies()
+        {
+            var referencedAssemblies = BuildManager.GetReferencedAssemblies();
+            IEnumerable<Type> typesCollected = Type.EmptyTypes;
+            foreach (Assembly assembly in referencedAssemblies)
+            {
+                Type[] typesInAsm;
+                try
+                {
+                    typesInAsm = assembly.GetTypes();
+                }
+                catch (ReflectionTypeLoadException ex)
+                {
+                    typesInAsm = ex.Types;
+                }
+
+                typesCollected = typesCollected.Concat(typesInAsm);
+            }
+
+            var rackConfigTypes = typesCollected.Where(type => TypeIsPublicClass(type) && TypeIsRackConfig(type));
+
+            Type rackConfig = rackConfigTypes.First();
+
+            return rackConfig;
+        }
+
+        private static bool TypeIsPublicClass(Type type)
+        {
+            return (type != null && type.IsPublic && type.IsClass && !type.IsAbstract);
+        }
+
+        private static bool TypeIsRackConfig(Type type)
+        {
+            return typeof(RackConfigBase).IsAssignableFrom(type);
+        }
     }
 }
